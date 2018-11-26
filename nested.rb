7b395e77c9aed41388052f6a08580a17117b6418
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-26 00:05:34 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
         }
      }
 
-		 programmer_hash[:dennis_ritchie][:languages]
+		 programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,7 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		programmer_hash[:yukihiro_matsumoto] = {:known_for => 'Ruby', :languages => ['LISP', 'C'] }
+		programmer_hash[:yukihiro_matsumoto] = { :known_for => 'Ruby', :languages => ['LISP', 'C'] }
 
 end
 
